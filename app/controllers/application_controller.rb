d22358bd4d564fcb0aad4fac8841b4a7cb583753
Frederico Araujo <fred.the.master@gmail.com>
2009-11-14 00:38:43 +0700

    fixed https, from using settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,12 +10,30 @@ class ApplicationController < ActionController::Base
   # Scrub sensitive parameters from your log
   filter_parameter_logging :password, :password_confirmation
 
-  ssl_allowed
+  before_filter :ensure_proper_protocol
   
   def login_required
     unless current_user
       flash[:error] = "Login Required"
       redirect_to "/session/new" 
+    end
+  end
+  
+  private
+  
+  def ssl_required?
+    Setting.get("HTTPS_ONLY") == "1"
+  end
+  
+  def ensure_proper_protocol
+    if ssl_required? && !request.ssl?
+      redirect_to "https://" + request.host + request.request_uri
+      flash.keep
+      return false
+    elsif request.ssl? && !ssl_required?
+      redirect_to "http://" + request.host + request.request_uri
+      flash.keep
+      return false
     end
   end
   
