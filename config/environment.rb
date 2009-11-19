20065c504767dcc39d7c0a6d87e27b0f6d55627c
Frederico Araujo <fred.the.master@gmail.com>
2009-11-19 19:07:15 +0700

    switch to gemcutter only

--- config/environment.rb
+++ config/environment.rb
@@ -20,9 +20,11 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "formtastic"
-  config.gem "thoughtbot-clearance",
-    :lib     => 'clearance',
-    :source  => 'http://gemcutter.org'
+  config.gem "clearance"
+  # Using gemcutter now. Steps:
+  # 1. gem install gemcutter
+  # 2. gem tumble 
+  
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
